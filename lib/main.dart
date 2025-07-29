e292a778cf8df6a27bc4621ecffa2f5350aefb3f
Dev.Emon <dev.emon.bd@gmail.com>
2025-07-29 13:04:53 +0600

    feat: integrate Firebase into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:quiz_app/ui/%20screens/splash_screen.dart';
+import 'package:quiz_app/firebase_options.dart';
+import 'app.dart';
 
-void main() {
+Future<void> main() async{
+WidgetsFlutterBinding.ensureInitialized();
+Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform
+);
   runApp(const QuizApp());
 }
 
-class QuizApp extends StatelessWidget {
-  const QuizApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Quiz App',
-      theme: ThemeData(
-        elevatedButtonTheme: _elevatedButtonThemeData(),
-        //inputDecorationTheme: _inputDecorationTheme(),
-      ),
-      home: SplashScreen(),
-    );
-  }
-}
-
-ElevatedButtonThemeData _elevatedButtonThemeData() {
-  return ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      fixedSize: Size(400, 45),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-    ),
-  );
-}
-
-InputDecorationTheme _inputDecorationTheme() {
-  return InputDecorationTheme(
-    filled: false,
-    hintStyle: TextStyle(color: Colors.white,fontSize: 17,fontWeight: FontWeight.bold),
-    border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.white),
-    ),
-    enabledBorder: UnderlineInputBorder(
-      borderSide: BorderSide(color: Colors.white),
-    ),
-  );
-}

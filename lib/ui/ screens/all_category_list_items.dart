89ff0166fc84ff131af951e5d88c898b390fe736
Dev.Emon <dev.emon.bd@gmail.com>
2025-08-09 06:01:53 +0600

    feat: Added Quiz Category & All Quiz List  Screen's

---
+++ lib/ui/ screens/all_category_list_items.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class AllCategoryListItems extends StatefulWidget {
+  const AllCategoryListItems({super.key});
+
+  @override
+  State<AllCategoryListItems> createState() => _AllCategoryItemsState();
+}
+
+class _AllCategoryItemsState extends State<AllCategoryListItems> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/ui/ screens/all_quiz_list_items.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class AllQuizListItems extends StatefulWidget {
+  const AllQuizListItems({super.key});
+
+  @override
+  State<AllQuizListItems> createState() => _AllQuizListItemsState();
+}
+
+class _AllQuizListItemsState extends State<AllQuizListItems> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
